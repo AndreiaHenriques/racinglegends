ab5301c2b44d94eac2de0f6c414cb81da7e63114
Andreia Henriques <estt13936@ipt.pt>
2018-04-13 22:19:59 +0100

    Ficheiros index.html, dadosAPI.js e logicaAPP.js criados. Bibliotecas bootstrap e jquery adicionadas.

---
+++ racingLegends/racingLegends/logicaAPP.js
@@ -0,0 +1,7 @@
+﻿
+// Registar o nosso "main" para quando o DOM está carregado.
+// O 'DOMContentLoaded' é executado ligeiramente antes do 'load',
+// logo, quando possível, deve ser usado.
+document.addEventListener('DOMContentLoaded', function main(e) {
+
+});
